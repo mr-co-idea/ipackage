9e5890cd68169417f75bb48181078a6524c9e943
Coles <17864211122@163.com>
2021-08-31 19:54:54 +0800

    modify: project-cli

--- merge/test/index.js
+++
@@ -1,9 +0,0 @@
-const m1 = require('./m1');
-const m2 = require('./m2');
-const { deepMerge } = require('..');
-const fs = require('fs');
-const path = require('path');
-
-const merge = deepMerge(Symbol.for('key'));
-
-fs.writeFileSync(path.join(__dirname, './result.json'), JSON.stringify(merge(m1, m2), undefined, 4))
